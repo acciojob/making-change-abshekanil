53900c19e8f48381aec4bc189d92d17651ed89a9
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2023-12-11 19:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const makeChange = (c) => {
   // your name here
+	const quarterValue = 25; 
+	const dimeValue = 10;
+	const nickelValue = 5;
+	const pennyValue = 1;
+
+	const q = Math.floor(c / quarterValue);
+
+	const remainingAfterQuarter = c % quarterValue;
+
+	const d = Math.floor(remainingAfterQuarter / dimeValue );
+
+	const remainingAfterDime = remainingAfterQuarter % dimeValue;
+
+	const n = Math.floor(remainingAfterDime / nickelValue);
+	const remainingAfterNickel = remainingAfterDime % nickelValue;
+
+	const p = Math.floor(remainingAfterNickel / pennyValue);
+ 
+	return {q, d, n, p};
 };
 
 // Do not the change the code below
